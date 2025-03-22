9978a7a1d5f9d0a75907e562212a8d739c48085f
Ceylin <caltepeceylin@gmail.com>
2025-03-22 15:09:04 +0300

    feat: add admin posts

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../../models/Category");
+const Post = require("../../models/Post");
+const path = require('path')
 
 router.get("/", (req, res) => {
   res.render("admin/admin");
@@ -29,5 +31,44 @@ router.delete("/categories/:id", (req, res) => {
     }
   });
 });
+
+router.get("/posts", (req, res) => {
+  Post.find({}).populate({path: 'category', model: Category}).sort({$natural: -1}).lean().then((posts) => {
+        res.render('admin/posts', {posts: posts});
+    })
+});
+
+router.delete("/posts/:id", (req, res) => {
+  Post.findByIdAndDelete(req.params.id).then((post, error) => {
+    if (!error) {
+      res.redirect("/admin/posts");
+    }
+  });
+});
+
+router.get("/posts/edit/:id", (req, res) => {
+  Post.findOne({_id: req.params.id}).lean().then((post, error) => {
+    Category.find({}).lean().then((categories) => {
+      res.render('admin/editpost', {post: post, categories: categories})
+    })
+  })
+});
+
+router.put('/posts/:id', (req, res) => {
+  let post_image = req.files.post_image
+  post_image.mv(path.resolve(__dirname, '../../public/img/postimages', post_image.name))
+
+  Post.findOne({_id: req.params.id}).then((post) => {
+    post.title = req.body.title
+    post.content = req.body.content
+    post.date = req.body.date
+    post.category = req.body.category
+    post.post_image = `/img/postimages/${post_image.name}`
+    
+    post.save().then(post => {
+      res.redirect("/admin/posts");
+    })
+  })
+})
 
 module.exports = router;
